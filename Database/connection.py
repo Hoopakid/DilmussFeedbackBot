d52c57323ecaf917960ff7b2468a28aedffdb63a
sherzod <azamovshavkat98@gmail.com>
2024-05-01 06:53:09 +0500

    project ready for deploy to server

--- Database/connection.py
+++ Database/connection.py
@@ -1,16 +1,23 @@
+import os
 import psycopg2
 from psycopg2 import extras
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_USER = os.environ.get('DB_USER')
+DB_NAME = os.environ.get('DB_NAME')
 
 
 def connection():
     conn = psycopg2.connect(
-        host='localhost',
-        port=5432,
-        dbname='dilmuss',
-        password='sherzodaaa123',
-        user='postgres'
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        password=DB_PASSWORD,
+        user=DB_USER
     )
     return conn
-
-
-
